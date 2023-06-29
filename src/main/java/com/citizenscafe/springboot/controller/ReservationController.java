052d8525b6c6f611bd20588d51b960505a2dee39
Michael Cooper <cooper.michaelcs@gmail.com>
2023-06-28 19:00:08 -0500

    set up POST rest endpoint for creating reservations

--- src/main/java/com/citizenscafe/springboot/controller/ReservationController.java
+++ src/main/java/com/citizenscafe/springboot/controller/ReservationController.java
@@ -3,13 +3,17 @@ package com.citizenscafe.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.citizenscafe.springboot.model.Reservation;
 import com.citizenscafe.springboot.repository.ReservationRepo;
 
+@CrossOrigin("*") 
 @RestController
 @RequestMapping("/api/v1/")
 public class ReservationController {
@@ -17,9 +21,17 @@ public class ReservationController {
 	@Autowired
 	private ReservationRepo reservationRepo;
 	
+	// get all reservations
 	@GetMapping("/reservations")
 	public List<Reservation> getReservations()
 	{
 		return reservationRepo.findAll();
+	}
+	
+	// create reservation
+	@PostMapping("/reservations")
+	public Reservation createReservation(@RequestBody Reservation reservation)
+	{
+		return reservationRepo.save(reservation);
 	}
 }
